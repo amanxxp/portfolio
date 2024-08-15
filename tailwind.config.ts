8a0a9cfbede9bdff562575018f9a66a3626c3a60
Aman Panwar <amanpanwar1672@gmail.com>
2024-08-15 20:53:22 +0530

    added the front page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        customGray: '#868686',
+        customGreen :'#00FF19',
+      },
+      
     },
   },
   plugins: [],
